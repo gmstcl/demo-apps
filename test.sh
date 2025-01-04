eec514a535a7e05de4d93626c7bc7a15d55fcb0d
EC2 Default User <ec2-user@ip-10-0-6-146.ap-northeast-2.compute.internal>
2025-01-04 14:56:34 +0000

    init

--- test.sh
+++ test.sh
@@ -8,7 +8,16 @@ echo '<?xml version="1.0" encoding="UTF-8"?>' > $REPORT_FILE
 echo '<testsuites>' >> $REPORT_FILE
 
 echo '<testsuite name="API Tests">' >> $REPORT_FILE
-curl -f http://localhost:8080/api/health
+curl -f http://localhost:8081/api
+if [ $? -eq 0 ]; then
+    echo '<testcase name="GET /api"/>' >> $REPORT_FILE
+else
+    echo '<testcase name="GET /api">' >> $REPORT_FILE
+    echo '<failure message="Failed to reach /api endpoint"/>' >> $REPORT_FILE
+    echo '</testcase>' >> $REPORT_FILE
+fi
+
+curl -f http://localhost:8081/api/health
 if [ $? -eq 0 ]; then
     echo '<testcase name="GET /api/health"/>' >> $REPORT_FILE
 else
@@ -17,7 +26,7 @@ else
     echo '</testcase>' >> $REPORT_FILE
 fi
 
-curl -f http://localhost:8080/api/time
+curl -f http://localhost:8081/api/time
 if [ $? -eq 0 ]; then
     echo '<testcase name="GET /api/time"/>' >> $REPORT_FILE
 else
